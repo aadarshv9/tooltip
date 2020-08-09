7c1bf018eebecd1a07165fbc09de541ada9b2b8a
aadarshv9 <aadarshv9@gmail.com>
2020-08-09 19:10:23 +0530

    Updates

---
+++ precache-manifest.75d274ed4c83ace394478b02417f29b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9389709d710b074c1bdda822863cf785",
+    "url": "/%7Btooltip%7D/index.html"
+  },
+  {
+    "revision": "db1a826f396c75ff3755",
+    "url": "/%7Btooltip%7D/static/css/main.66f28ffa.chunk.css"
+  },
+  {
+    "revision": "39f2c1537d206e9168ea",
+    "url": "/%7Btooltip%7D/static/js/2.0e2e7a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7Btooltip%7D/static/js/2.0e2e7a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1a826f396c75ff3755",
+    "url": "/%7Btooltip%7D/static/js/main.7e9d7d06.chunk.js"
+  },
+  {
+    "revision": "660035485575ef1f3b0a",
+    "url": "/%7Btooltip%7D/static/js/runtime-main.7dd57424.js"
+  }
+]);
